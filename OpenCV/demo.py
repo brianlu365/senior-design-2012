fdb3c70ef581c490605a951414333039a9265385
Hong Xiang Wang <whx29@hotmail.com>
2012-10-27 23:36:08 -0700

    tried out with opencv.

---
+++ OpenCV/demo.py
@@ -0,0 +1,28 @@
+import cv2
+
+# im = cv2.imread('lena.jpg')
+# im_lowres = cv2.pyrDown(im)
+# gray = cv2.cvtColor(im_lowres,cv2.COLOR_RGB2GRAY)
+
+# s = cv2.SURF()
+# mask = uint8(ones(gray.shape))
+# keypoints = s.detect(gray,mask)
+
+# vis = cv2.cvtColor(gray,cv2.COLOR_GRAY2BGR)
+# for k in keypoints[::10]:
+#   cv2.circle(vis,(int(k.py[0]),int(k.pt[1])),2,(0,255,0),-1)
+#   cv2.circle(vis,(int(k.py[0]),int(k.pt[1])),int(k.size),(0,255,0),2)
+# cv2.imshow('demo',vis)
+# cv2.waitKey()
+
+cap = cv2.VideoCapture(0)
+
+while True:
+  ret,im = cap.read()
+  blue = cv2.GaussianBlur(im,(0,0),5)
+  cv2.imshow('video test',blur)
+  key = cv2.waitKey(10)
+  if key == 27:
+    break
+  if key == ord(' '):
+    cv2.imwrite('vid_result.jpg',im)
